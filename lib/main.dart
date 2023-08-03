4659320c8faa67c72285ddaef5d1ccc25d1aa034
Apryan27 <aprythomas5@gmail.com>
2023-08-03 22:26:01 +0300

    first full commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/rie.jpg'),
+              ),
+              Text(
+                'Aprian Rie',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Mortal Kombat Enthusiast',
+                style: TextStyle(
+                    fontFamily: 'Inconsolata',
+                    color: Colors.teal[100],
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+276 456 898',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Inconsolata',
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'apryrie@hotmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'Inconsolata',
+                          color: Colors.teal[900]),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
